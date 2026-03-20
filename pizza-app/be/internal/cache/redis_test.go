47c89c3b1932b2d1e6036a90342f72486954a829
gianglt1 <gianglt2198@gmail.com>
2026-03-20 21:46:25 +0700

    add unit test

--- pizza-app/be/internal/cache/redis_test.go
+++ pizza-app/be/internal/cache/redis_test.go
@@ -30,7 +30,7 @@ func TestRedisCache_Delete(t *testing.T) {
 	cache := NewRedisCache(s.Addr(), "")
 	ctx := context.Background()
 
-	cache.Set(ctx, "test_key", "test_value", 1*time.Minute)
+	_ = cache.Set(ctx, "test_key", "test_value", 1*time.Minute)
 
 	err := cache.Delete(ctx, "test_key")
 	assert.NoError(t, err)
